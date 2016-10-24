0cca0651a96fefd9cc3a538ebd129a06e1467281
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2016-10-24 10:22:26 +0800

    add Ch5 examples class

---
+++ com/jsmean/Manager.java
@@ -0,0 +1,28 @@
+package LearningJava.com.jsmean;
+
+/**
+ * Created by mike on 16-10-24.
+ */
+import LearningJava.com.jsmean.Employee;
+
+public class Manager extends Employee {
+    private double bonus;
+    public void setBonus(double d){
+        this.bonus=d;
+    }
+    public double getSalary(){
+        return super.getSalary()+bonus;
+    }
+    public Manager(String name,double salary){
+        super(name,salary);
+        this.bonus=0;
+    }
+    public Manager(String name,double salary,int year,int month,int day) {
+        super(name, salary, year, month, day);
+        this.bonus = 0;
+    }
+    public Manager(String name,double salary,int year,int month,int day,double bonus) {
+        super(name, salary, year, month, day);
+        this.bonus = bonus;
+    }
+}
